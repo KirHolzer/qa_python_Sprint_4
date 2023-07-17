5344421c282b03ecf7ce0921d05d0f2f2a0ec6a2
Кирилл Гольцер <kirill.holzer@yandex.ru>
2023-07-17 15:47:10 +0300

    REFACTOR: assert AboutRentPage(browser) =>assert about_rent_page. in all tests classes

--- tests/test_about_rent_page.py
+++ tests/test_about_rent_page.py
@@ -14,7 +14,8 @@ class TestAboutRentPage:
     @allure.description('Нажимаем на логотип "Самоката" и переходим на главную страницу')
     def test_return_to_main_by_click_on_scooter_in_header(self, browser):
         OrderPage(browser).open_about_rent_page()
-        AboutRentPage(browser).click_on_scooter_in_header()
+        about_rent_page = AboutRentPage(browser)
+        about_rent_page.click_on_scooter_in_header()
         WebDriverWait(browser, 10).until(
             expected_conditions.presence_of_element_located(MainPaigeLocators.MAIN_PAGE_HEADING))
         assert browser.current_url == TestUrls.MainPageUrl
@@ -23,31 +24,34 @@ class TestAboutRentPage:
     @allure.description('Нажимаем на кнопку "назад" и переходим на страницу заполнения клиентских данных')
     def test_click_on_back_button(self, browser):
         OrderPage(browser).open_about_rent_page()
-        AboutRentPage(browser).click_on_back_button()
+        about_rent_page = AboutRentPage(browser)
+        about_rent_page.click_on_back_button()
         WebDriverWait(browser, 10).until(
             expected_conditions.presence_of_element_located(OrderPageLocators.HEADING_ON_ORDER_PAGE))
         expected_text = 'Для кого самокат'
-        assert AboutRentPage(browser).find_text(OrderPageLocators.HEADING_ON_ORDER_PAGE) == expected_text
+        assert about_rent_page.find_text(OrderPageLocators.HEADING_ON_ORDER_PAGE) == expected_text
 
     @allure.title('Проверка перехода на модальную страницу подтверждения заказа')
     @allure.description('Заполняем поля "Про аренду" , переходим на модальную страницу подтверждения заказа')
     def test_go_to_modal_window(self, browser):
         OrderPage(browser).open_about_rent_page()
-        AboutRentPage(browser).go_to_confirmation_modal_window()
+        about_rent_page = AboutRentPage(browser)
+        about_rent_page.go_to_confirmation_modal_window()
         WebDriverWait(browser, 10).until(
             expected_conditions.presence_of_element_located(AboutRentLocators.YES_BUTTON_MODAL))
         expected_text = 'Хотите оформить заказ?'
-        assert AboutRentPage(browser).find_text(AboutRentLocators.HEADING_CONFIRMATION_MODAL).strip() == expected_text
+        assert about_rent_page.find_text(AboutRentLocators.HEADING_CONFIRMATION_MODAL).strip() == expected_text
 
     @allure.title('Проверка возврата на страницу "Про аренду" с модальноко окна подтверждения заказа')
     @allure.description('Заполняем поля "Про аренду" , переходим на модальную окно подтверждения заказа')
     def test_return_to_about_rent_from_modal(self, browser):
         OrderPage(browser).open_about_rent_page()
-        AboutRentPage(browser).click_no_button_on_confirmation_modal_window()
+        about_rent_page = AboutRentPage(browser)
+        about_rent_page.click_no_button_on_confirmation_modal_window()
         WebDriverWait(browser, 10).until(
             expected_conditions.presence_of_element_located(AboutRentLocators.BODY_ORDER_BUTTON))
         expected_text = 'Про аренду'
-        assert AboutRentPage(browser).find_text(AboutRentLocators.HEADING_ON_ABOUT_PAGE) == expected_text
+        assert about_rent_page.find_text(AboutRentLocators.HEADING_ON_ABOUT_PAGE) == expected_text
 
     @allure.title('Проверка оформления заказа через кнопку "Заказать" в правом верхнем углу главной страницы')
     @allure.description('Нажимаем на кнопку "Заказать" в правом верхнем углу страницы и проходим весь положительный '
@@ -56,11 +60,12 @@ class TestAboutRentPage:
         MainPage(browser).open_main_url()
         MainPage(browser).click_on_order_button_in_heeader()
         OrderPage(browser).open_about_rent_page()
-        AboutRentPage(browser).click_yes_button_on_confirmation_modal_window()
+        about_rent_page = AboutRentPage(browser)
+        about_rent_page.click_yes_button_on_confirmation_modal_window()
         WebDriverWait(browser, 10).until(
             expected_conditions.presence_of_element_located(AboutRentLocators.LOOK_AT_STATUS))
         expected_text = 'Посмотреть статус'
-        assert AboutRentPage(browser).find_text(AboutRentLocators.LOOK_AT_STATUS) == expected_text
+        assert about_rent_page.find_text(AboutRentLocators.LOOK_AT_STATUS) == expected_text
 
     @allure.title('Проверка оформления заказа через кнопку "Заказать" в нижней части главной страницы')
     @allure.description('Нажимаем на кнопку "Заказать" в нижней части главной страницы и проходим весь положительный '
@@ -70,8 +75,9 @@ class TestAboutRentPage:
         MainPage(browser).scroll_to_order_button_in_body()
         MainPage(browser).click_on_order_button_in_body()
         OrderPage(browser).open_about_rent_page()
-        AboutRentPage(browser).click_yes_button_on_confirmation_modal_window()
+        about_rent_page = AboutRentPage(browser)
+        about_rent_page.click_yes_button_on_confirmation_modal_window()
         WebDriverWait(browser, 10).until(
             expected_conditions.presence_of_element_located(AboutRentLocators.LOOK_AT_STATUS))
         expected_text = 'Посмотреть статус'
-        assert AboutRentPage(browser).find_text(AboutRentLocators.LOOK_AT_STATUS) == expected_text
+        assert about_rent_page.find_text(AboutRentLocators.LOOK_AT_STATUS) == expected_text
--- tests/test_order_page.py
+++ tests/test_order_page.py
@@ -12,8 +12,9 @@ class TestOrderPage:
     @allure.title('Проверка перехода на главную страницу по клику на "Самокат" со страницы заказа')
     @allure.description('Нажимаем на логотип "Самоката" и переходим на главную страницу')
     def test_return_to_main_by_click_on_scooter_in_header(self, browser):
-        OrderPage(browser).open_order_url()
-        OrderPage(browser).click_on_scooter_in_header()
+        order_page = OrderPage(browser)
+        order_page.open_order_url()
+        order_page.click_on_scooter_in_header()
         WebDriverWait(browser, 10).until(
             expected_conditions.presence_of_element_located(MainPaigeLocators.MAIN_PAGE_HEADING))
         assert browser.current_url == TestUrls.MainPageUrl
@@ -21,61 +22,67 @@ class TestOrderPage:
     @allure.title('Проверка перехода на страницу "Про аренду" по клику на кнопку "Далее" после успешного заполнения клиентскими данными')
     @allure.description('Заполнить данные клиента и перейти на страницу "Про аренду"')
     def test_fill_client_info_succsesfully(self, browser):
-        OrderPage(browser).open_order_url()
-        OrderPage(browser).fill_client_info()
-        OrderPage(browser).fill_metro_field_by_click()
-        OrderPage(browser).click_on_button_forward()
+        order_page = OrderPage(browser)
+        order_page.open_order_url()
+        order_page.fill_client_info()
+        order_page.fill_metro_field_by_click()
+        order_page.click_on_button_forward()
         WebDriverWait(browser, 10).until(
             expected_conditions.presence_of_element_located(AboutRentLocators.HEADING_ON_ABOUT_PAGE))
         expected_text = 'Про аренду'
-        assert OrderPage(browser).find_text(AboutRentLocators.HEADING_ON_ABOUT_PAGE) == expected_text
+        assert order_page.find_text(AboutRentLocators.HEADING_ON_ABOUT_PAGE) == expected_text
 
     @allure.title('Проверка ошибки на валидацию поля "Имя"')
     @allure.description('Оставить поле "Имя" пустым и кликнуть "Далее"')
     def test_validate_error_by_input_first_name(self, browser):
-        OrderPage(browser).open_order_url()
-        OrderPage(browser).click_on_button_forward()
+        order_page = OrderPage(browser)
+        order_page.open_order_url()
+        order_page.click_on_button_forward()
         WebDriverWait(browser, 10).until(
             expected_conditions.presence_of_element_located(OrderPageLocators.VALIDATE_ERROR_FIRST_NAME))
         expected_text = 'Введите корректное имя'
-        assert OrderPage(browser).find_text(OrderPageLocators.VALIDATE_ERROR_FIRST_NAME) == expected_text
+        assert order_page.find_text(OrderPageLocators.VALIDATE_ERROR_FIRST_NAME) == expected_text
 
     @allure.title('Проверка ошибки на валидацию поля "Фамилия"')
     @allure.description('Оставить поле "Фамилия" пустым и кликнуть "Далее"')
     def test_validate_error_by_input_second_name(self, browser):
-        OrderPage(browser).open_order_url()
-        OrderPage(browser).click_on_button_forward()
+        order_page = OrderPage(browser)
+        order_page.open_order_url()
+        order_page.click_on_button_forward()
         WebDriverWait(browser, 10).until(
             expected_conditions.presence_of_element_located(OrderPageLocators.VALIDATE_ERROR_SECOND_NAME))
         expected_text = 'Введите корректную фамилию'
-        assert OrderPage(browser).find_text(OrderPageLocators.VALIDATE_ERROR_SECOND_NAME) == expected_text
+        assert order_page.find_text(OrderPageLocators.VALIDATE_ERROR_SECOND_NAME) == expected_text
 
     @allure.title('Проверка ошибки на валидацию поля "* Адрес: куда привезти заказ"')
     @allure.description('Оставить поле "* Адрес: куда привезти заказ" пустым и кликнуть "Далее"')
     def test_validate_error_by_input_delivery_address(self, browser):
-        OrderPage(browser).open_order_url()
-        OrderPage(browser).click_on_button_forward()
+        order_page = OrderPage(browser)
+        order_page.open_order_url()
+        order_page.click_on_button_forward()
         WebDriverWait(browser, 10).until(
             expected_conditions.presence_of_element_located(OrderPageLocators.VALIDATE_ERROR_FIRST_NAME))
         expected_text = 'Введите корректный адрес'
-        assert OrderPage(browser).find_text(OrderPageLocators.VALIDATE_ERROR_DELIVERY_ADDRESS) == expected_text
+        assert order_page.find_text(OrderPageLocators.VALIDATE_ERROR_DELIVERY_ADDRESS) == expected_text
 
     @allure.title('Проверка ошибки на валидацию поля "* Станция метро"')
     @allure.description('Оставить поле "* Станция метро" пустым и кликнуть "Далее"')
     def test_validate_error_by_input_metro_station(self, browser):
-        OrderPage(browser).open_order_url()
-        OrderPage(browser).click_on_button_forward()
+        order_page = OrderPage(browser)
+        order_page.open_order_url()
+        order_page.click_on_button_forward()
         WebDriverWait(browser, 10).until(
             expected_conditions.presence_of_element_located(OrderPageLocators.VALIDATE_ERROR_METRO_STATION))
         expected_text = 'Выберите станцию'
-        assert OrderPage(browser).find_text(OrderPageLocators.VALIDATE_ERROR_METRO_STATION) == expected_text
+        assert order_page.find_text(OrderPageLocators.VALIDATE_ERROR_METRO_STATION) == expected_text
 
     @allure.title('Проверка ошибки на валидацию поля "* Номера телефона"')
     @allure.description('Оставить поле "* Номера телефона" пустым и кликнуть "Далее"')
     def test_validate_error_by_input_phone_number(self, browser):
-        OrderPage(browser).open_order_url()
-        OrderPage(browser).click_on_button_forward()
+        order_page = OrderPage(browser)
+        order_page.open_order_url()
+        order_page.click_on_button_forward()
         WebDriverWait(browser, 10).until(
             expected_conditions.presence_of_element_located(OrderPageLocators.VALIDATE_ERROR_PHONE_NUMBER))
         expected_text = 'Введите корректный номер'
-        assert OrderPage(browser).find_text(OrderPageLocators.VALIDATE_ERROR_PHONE_NUMBER) == expected_text
+        assert order_page.find_text(OrderPageLocators.VALIDATE_ERROR_PHONE_NUMBER) == expected_text
